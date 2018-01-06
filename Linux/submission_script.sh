788025f98bc684a2d6593d6813d6b5f385311edf
unknown <tsemargaretld50@gmail.com>
2018-01-05 19:23:43 -0800

    add rate codes: state based

--- Linux/submission_script.sh
+++ Linux/submission_script.sh
@@ -44,4 +44,42 @@ model_name="/home/usr/BioNetGen_files/models/schlogl.net"
 #toc: the current total computational time
 #ijk: the current ijk iteration of transition matrix calculation
 
+#Also ouputs a temporary voronoi.txt, transition_matrix_count.txt and transition_matrix.txt file for every 2nd ijk iteration under output_save_file/output_save_name/
+#containing the voronoi centeres, current transition matrix calculated from i->j counts, and current transition matrix calcluated from i-> weights
 
+
+
+#Runs the rate calculation with previously definited Voronoi centers and uses a region of interest definition where region1 and region 2 are selections of Voronoi centers
+#The number of simulation iterations used to calculate the rate  matrix
+transition_rate_loops="2000" 
+
+#The file location specifying the original voronoi centers
+original_voronoi_center="voronoi_centers.txt"
+
+#The file location that maps each voronoi center to a region e.g. from 0..10
+mapping_centers="map.txt"
+
+#The voronoi centers mapping to region1 are the 1st region of interest
+region1="1" 
+
+#The voronoi centers mapping to region2 are the 2nd region of interest
+region2="2" 
+
+#Change the matlab call to your MATLABROOT and uncomment if you want to start a rate calculation
+#/usr/local/MATLAB/R2012a/bin/matlab -nodisplay -r "try,  General_bin_movement_and_rates_voronoi_bins_local_linux(${transition_rate_loops},${temporary_save_location},${Replicas_per_bins},${output_file_name},${tau},${species},${output_save_file},${BNG_root},${model_name},${original_voronoi_center},${mapping_centers},${region1},${region2}),catch,exit(0),end,exit(1)"
+
+#Outputs a matfile: output_file_name.mat
+#Inside the matfile, the output contains:
+#replicas: the current replica positions, tags, and replica weights
+#rates: a matlab struct file containing the sub_variables: 
+#     total_number_of_replicas_transferred: the total number of replicas transferred from 2 -> 1 (in the first index) and 1 -> 2 (in the 2nd index)
+#     total_weight_of_replicas_transferred: the total number of replicas transferred from 2 -> 1 (in the first index) and 1 -> 2 (in the 2nd index)
+#     total_probability_in_region_i
+#     tau
+
+#iteration: the current 0-4 temporary iteration number for temporarily stored replica information
+#toc: the current total computational time
+#ijk: the current ijk iteration of transition matrix calculation
+
+#Also ouputs a temporary MFPT.txt and rate.txt file for under output_save_file/output_save_name/
+#containing the MFPT and rates for every ijk iteration
